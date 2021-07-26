263e7f192d4d71ad032f6640bb16666aeb7d1fee
maximan81 <ericmaximan@gmail.com>
2021-07-26 16:04:55 +0300

    start hard testing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
 require __DIR__.'./../vendor/autoload.php';
+
+use App\MonthPeriod;
+
+$monthPeriod = new MonthPeriod();
+
+$startAbsencePeriod = new DateTime('last day of previous month 00:00:00');
+$endAbsencePeriod = new DateTime('fist day of this month 23:59:99');
